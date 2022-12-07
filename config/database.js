5d325532937deff9a258f83112f24a1dcea41ace
Weronika Derkowska <weronika.derkowska@gmail.com>
2022-12-07 01:53:48 +0100

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,35 @@
-const path = require('path');
+const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        schema: env("DATABASE_SCHEMA", "public"), // Not required
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        },
+      },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
